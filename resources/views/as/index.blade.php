d6eacf6bf9a8302c2b7fb55049ea998b1a0e5ca5
imvishalnag <imvishalnag@gmail.com>
2020-11-30 13:11:02 +0530

    update

--- resources/views/as/index.blade.php
+++ resources/views/as/index.blade.php
@@ -797,7 +797,7 @@
                         <h3 class="block-title"><span>Facebook</span></h3>
 
                         <div class="widget text-center">
-                            <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebook&tabs=timeline&width=340&height=600&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="600" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+                            <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Finvoicene&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=318843728990066" width="340" height="600" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
                         </div><!-- Sidebar Ad end -->
                     </div><!--Sidebar right end -->
                 </div><!-- Sidebar col end -->
