afd440a26b82b75307daff52f023e0db0206b403
Saddam Hussain <sishack8@gmail.com>
2020-12-25 15:24:57 +0530

    test

--- storage/framework/views/6e3828317c00c493a19ae831ad96028db7d4fed7.php
+++ storage/framework/views/6e3828317c00c493a19ae831ad96028db7d4fed7.php
@@ -1,11 +1,11 @@
 
-@extends('admin.template.admin_master')
 
-@section('content')
+
+<?php $__env->startSection('content'); ?>
     <!-- page content -->
 <div class="right_col" role="main">
     <div class="row">
-            {{-- <div class="col-md-2"></div> --}}
+            
             <div class="col-md-12" style="margin-top:50px;">
                 <div class="x_panel">
 
@@ -14,26 +14,30 @@
                         <div class="clearfix"></div>
                     </div>
                 <div>
-                     @if (Session::has('message'))
-                        <div class="alert alert-success" >{{ Session::get('message') }}</div>
-                     @endif
-                     @if (Session::has('error'))
-                        <div class="alert alert-danger">{{ Session::get('error') }}</div>
-                     @endif
+                     <?php if(Session::has('message')): ?>
+                        <div class="alert alert-success" ><?php echo e(Session::get('message')); ?></div>
+                     <?php endif; ?>
+                     <?php if(Session::has('error')): ?>
+                        <div class="alert alert-danger"><?php echo e(Session::get('error')); ?></div>
+                     <?php endif; ?>
                 </div>
                     <div>
                         <div class="x_content">
-                        {{ Form::open(['method' => 'post','route'=>'admin.add_post', 'enctype'=>'multipart/form-data']) }}
+                        <?php echo e(Form::open(['method' => 'post','route'=>'admin.add_post', 'enctype'=>'multipart/form-data'])); ?>
+
                             <div class="well" style="overflow: auto">
                                 <div class="form-row mb-10">
                                     <div class="col-md-4 col-sm-12 col-xs-12 mb-3">
                                         <label for="title">Title</label>
-                                        <input type="text" class="form-control" name="title" placeholder="Enter Title" value="{{old('title')}}">
-                                        @if($errors->has('title'))
+                                        <input type="text" class="form-control" name="title" placeholder="Enter Title" value="<?php echo e(old('title')); ?>">
+                                        <?php if($errors->has('title')): ?>
                                         <span class="invalid-feedback" role="alert" style="color:red">
-                                            <strong>{{ $errors->first('title') }}</strong>
+                                            <strong><?php echo e($errors->first('title')); ?></strong>
                                         </span>
-                                        @enderror
+                                        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
                                     </div>
                                     <div class="col-md-4 col-sm-6 col-xs-12 mb-3">
                                         <label for="type">Type</label>
@@ -42,20 +46,26 @@
                                             <option value="1" >English</option>
                                             <option value="2" >Assamese</option>
                                         </select>
-                                        @if($errors->has('type'))
+                                        <?php if($errors->has('type')): ?>
                                         <span class="invalid-feedback" role="alert" style="color:red">
-                                            <strong>{{ $errors->first('type') }}</strong>
+                                            <strong><?php echo e($errors->first('type')); ?></strong>
                                         </span>
-                                        @enderror
+                                        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
                                     </div>
                                     <div class="col-md-4 col-sm-6 col-xs-12 mb-3">
                                         <label for="author">Author</label>
-                                        <input type="text" class="form-control" value="{{old('author')}}" name="author" placeholder="Author">
-                                        @if($errors->has('author'))
+                                        <input type="text" class="form-control" value="<?php echo e(old('author')); ?>" name="author" placeholder="Author">
+                                        <?php if($errors->has('author')): ?>
                                         <span class="invalid-feedback" role="alert" style="color:red">
-                                            <strong>{{ $errors->first('author') }}</strong>
+                                            <strong><?php echo e($errors->first('author')); ?></strong>
                                         </span>
-                                        @enderror
+                                        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
                                     </div>
                                 </div>
                                 <div class="form-row mb-10">
@@ -63,26 +73,32 @@
                                         <label for="category">Category</label>
                                         <select class="form-control" name="category" id="category">
                                             <option selected="" disabled="" value="">Select Category</option>
-                                            @if(isset($categorys) && !empty($categorys) && count($categorys) > 0)
-                                                @foreach($categorys as $category)
-                                                    <option value="{{$category->id}}">{{$category->category_name}}</option>
-                                                @endforeach
-                                            @endif
+                                            <?php if(isset($categorys) && !empty($categorys) && count($categorys) > 0): ?>
+                                                <?php $__currentLoopData = $categorys; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $category): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                                    <option value="<?php echo e($category->id); ?>"><?php echo e($category->category_name); ?></option>
+                                                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                                            <?php endif; ?>
                                         </select>
-                                        @if($errors->has('category'))
+                                        <?php if($errors->has('category')): ?>
                                         <span class="invalid-feedback" role="alert" style="color:red">
-                                            <strong>{{ $errors->first('category') }}</strong>
+                                            <strong><?php echo e($errors->first('category')); ?></strong>
                                         </span>
-                                        @enderror
+                                        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
                                     </div>
                                     <div class="col-md-6 col-sm-12 col-xs-12 mb-3">
                                         <label for="image">Image Upload</label>
                                         <input type="file" class="form-control" name="image" accept="/*">
-                                        @if($errors->has('image'))
+                                        <?php if($errors->has('image')): ?>
                                         <span class="invalid-feedback" role="alert" style="color:red">
-                                            <strong>{{ $errors->first('image') }}</strong>
+                                            <strong><?php echo e($errors->first('image')); ?></strong>
                                         </span>
-                                        @enderror
+                                        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
                                     </div>
                                 </div>                
                                 </div>
@@ -91,34 +107,38 @@
                                 <div class="form-row mb-10">
                                     <div class="col-md-12 col-sm-12 col-xs-12 mb-3">
                                         <label for="category">Body*</label>
-                                        <textarea class="form-control" name="body" placeholder="Enter full description" id="body">{{old('body')}}</textarea>
-                                        @if($errors->has('body'))
+                                        <textarea class="form-control" name="body" placeholder="Enter full description" id="body"><?php echo e(old('body')); ?></textarea>
+                                        <?php if($errors->has('body')): ?>
                                         <span class="invalid-feedback" role="alert" style="color:red">
-                                            <strong>{{ $errors->first('body') }}</strong>
+                                            <strong><?php echo e($errors->first('body')); ?></strong>
                                         </span>
-                                        @enderror
+                                        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
                                     </div>                                 
                                 </div>
                             </div>
                                 <div class="form-group">    	            	
-                                    {{ Form::submit('Publish', array('class'=>'btn btn-success pull-right')) }}  
+                                    <?php echo e(Form::submit('Publish', array('class'=>'btn btn-success pull-right'))); ?>  
                                 </div>
-                                {{ Form::close() }}
+                                <?php echo e(Form::close()); ?>
+
                         </div>
                     </div>
                 </div>
             </div>
     </div>
 </div>
-@endsection
+<?php $__env->stopSection(); ?>
 
-@section('script')
+<?php $__env->startSection('script'); ?>
 
- <script src="{{ asset('ckeditor4/ckeditor.js')}}"></script>
+ <script src="<?php echo e(asset('ckeditor4/ckeditor.js')); ?>"></script>
 <script>
     CKEDITOR.replace( 'body', {
         height: 400,
-        filebrowserUploadUrl: "{{route('admin.ck_editor_image_upload', ['_token' => csrf_token() ])}}",
+        filebrowserUploadUrl: "<?php echo e(route('admin.ck_editor_image_upload', ['_token' => csrf_token() ])); ?>",
         filebrowserUploadMethod: 'form'
     } );
 
@@ -130,7 +150,7 @@
                     }
                 });
             $.ajax({
-                url: "{{route('admin.populate_category')}}",
+                url: "<?php echo e(route('admin.populate_category')); ?>",
                 method: "GET",
                 data: {query:query},
                 success: function(data){
@@ -151,6 +171,8 @@
         });
     });
 </script>
- @endsection
+ <?php $__env->stopSection(); ?>
 
 
+
+<?php echo $__env->make('admin.template.admin_master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\invoicene\resources\views/admin/create_post.blade.php ENDPATH**/ ?>
