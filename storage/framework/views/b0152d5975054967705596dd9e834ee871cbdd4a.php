24d0c59e945b3e174b52b49dd2b299f2cbbaeb8f
imvishalnag <imvishalnag@gmail.com>
2020-12-29 18:48:02 +0530

    update

---
+++ storage/framework/views/b0152d5975054967705596dd9e834ee871cbdd4a.php
@@ -0,0 +1,47 @@
+
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+	<link rel="icon" href="images/favicon.ico" type="image/ico" />
+
+    <title>INVOICE NE</title>
+
+    <!-- Bootstrap -->
+    <link href="<?php echo e(asset('vendors/bootstrap/dist/css/bootstrap.min.css')); ?>" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="<?php echo e(asset('vendors/font-awesome/css/font-awesome.min.css')); ?>" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="<?php echo e(asset('vendors/nprogress/nprogress.css')); ?>" rel="stylesheet">
+    <!-- iCheck -->
+    <link href="<?php echo e(asset('vendors/iCheck/skins/flat/green.css')); ?>" rel="stylesheet">
+	
+    <!-- bootstrap-progressbar -->
+    <link href="<?php echo e(asset('vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css')); ?>" rel="stylesheet">
+    <!-- JQVMap -->
+    <link href="<?php echo e(asset('vendors/jqvmap/dist/jqvmap.min.css')); ?>" rel="stylesheet"/>
+    <!-- bootstrap-daterangepicker -->
+    <link href="<?php echo e(asset('vendors/bootstrap-daterangepicker/daterangepicker.css')); ?>" rel="stylesheet">
+
+    <link href="<?php echo e(asset('vendors/datatables.net-bs/css/dataTables.bootstrap.min.css')); ?>" rel="stylesheet">
+    <link href="<?php echo e(asset('vendors/datatables.net-buttons-bs/css/buttons.bootstrap.min.css')); ?>" rel="stylesheet">
+    <link href="<?php echo e(asset('vendors/datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.min.css')); ?>" rel="stylesheet">
+    <link href="<?php echo e(asset('vendors/datatables.net-responsive-bs/css/responsive.bootstrap.min.css')); ?>" rel="stylesheet">
+    <link href="<?php echo e(asset('vendors/datatables.net-scroller-bs/css/scroller.bootstrap.min.css')); ?>" rel="stylesheet">
+
+    <!-- Custom Theme Style -->
+    <link href="<?php echo e(asset('build/css/custom.min.css')); ?>" rel="stylesheet">
+
+    <?php echo $__env->yieldContent('css'); ?>
+  </head>
+  <body class="<?php echo e($class='nav-md' ?? ''); ?>">
+    <div class="container body">
+      <div class="main_container">
+        <!-- Left Naviagtion-->
+        <?php echo $__env->make('admin.include.leftnav', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+        <?php echo $__env->make('admin.include.topnav', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH E:\xampp\htdocs\invoice\resources\views/admin/include/header.blade.php ENDPATH**/ ?>
