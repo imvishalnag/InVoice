240f798b1ffda25ed3cf20d845db11f1b73db807
imvishalnag <imvishalnag@gmail.com>
2020-12-29 19:06:38 +0530

    update

--- resources/views/admin/include/leftnav.blade.php
+++ resources/views/admin/include/leftnav.blade.php
@@ -8,9 +8,6 @@
 
       <!-- menu profile quick info -->
       <div class="profile clearfix">
-        <div class="profile_pic">
-          <img src="{{asset('production/images/img.jpg')}}" alt="..." class="img-circle profile_img">
-        </div>
         <div class="profile_info">
           <span>Welcome,</span>
           <h2>{{Auth::user()->name}}</h2> 
