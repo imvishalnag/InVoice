bdb6f44752a2b61632ce350999084e12ae36c4f3
imvishalnag <imvishalnag@gmail.com>
2020-12-29 18:35:59 +0530

    change passwird added in admin panel

--- storage/framework/views/d68969ef5a0b20fd628dde2d66155eafeeddc69d.php
+++
@@ -1,4 +0,0 @@
-<?php echo $__env->make('admin.include.header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-    <?php echo $__env->yieldContent('content'); ?>
-<?php echo $__env->make('admin.include.footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<?php echo $__env->yieldContent('script'); ?><?php /**PATH E:\xampp\htdocs\newstimenortheast\resources\views/admin/template/admin_master.blade.php ENDPATH**/ ?>
